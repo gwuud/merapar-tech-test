99ef89246f0c35b00ed86d7d0bf4b7c31a1aad3a
Gareth Wood <gareth.wuud@icloud.com>
2023-11-22 13:22:25 +0000

    Clean Bootstrap

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -4,27 +4,23 @@ set -o pipefail
 
 PROJECT=merapar-tech-test
 GOVER=1.21.3
+EXEC=bin/server-dist
+RUN_AS=ec2-user
 
 trap 'exit 1' ERR
 
-echo -e "\nInstalling Dependencies..."
 yum update -y
 amazon-linux-extras install ansible2 -y
 yum install -y git
 
-# install go from source (yum is behind brew)
-# from https://go.dev/doc/install
-echo -e "\nInstalling Golang..."
 wget https://go.dev/dl/go${GOVER}.linux-amd64.tar.gz
 rm -rf /usr/local/go && \
 tar -C /usr/local -xzf go${GOVER}.linux-amd64.tar.gz
 
 export PATH=$PATH:/usr/local/go/bin
 
-# get application source (ansible-pull not working for subdirs(?))
-echo -e "\nCloning Repository..."
-git clone https://github.com/gwuud/${PROJECT}.git
+su $RUN_AS -c "git clone https://github.com/gwuud/${PROJECT}.git \
+	/home/$RUN_AS/$PROJECT"
 
-# build and run application
-cd $PROJECT/application || exit 1
-make bin/server-dist && ./bin/server-dist
+cd /home/$RUN_AS/$PROJECT/application || exit 1
+su $RUN_AS -c "make $EXEC && ./$EXEC &"
