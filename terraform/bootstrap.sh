9fe3d516a72c42b002babd79bcdb0f7b3ef5f259
Gareth Wood <gareth.wuud@icloud.com>
2023-11-22 13:49:41 +0000

    Tidy Bootstrap

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -3,24 +3,31 @@
 set -o pipefail
 
 PROJECT=merapar-tech-test
+OWNER=gwuud
+
 GOVER=1.21.3
 EXEC=bin/server-dist
 RUN_AS=ec2-user
 
 trap 'exit 1' ERR
 
+echo -e '\nInstalling Dependencies...'
 yum update -y
 amazon-linux-extras install ansible2 -y
 yum install -y git
 
+echo -e '\nInstalling Golang...'
 wget https://go.dev/dl/go${GOVER}.linux-amd64.tar.gz
 rm -rf /usr/local/go && \
 tar -C /usr/local -xzf go${GOVER}.linux-amd64.tar.gz
 
 export PATH=$PATH:/usr/local/go/bin
 
-su $RUN_AS -c "git clone https://github.com/gwuud/${PROJECT}.git \
+echo -e '\nCloning Application Source Code...'
+su $RUN_AS -c "git clone https://github.com/${OWNER}/${PROJECT}.git \
 	/home/$RUN_AS/$PROJECT"
 
 cd /home/$RUN_AS/$PROJECT/application || exit 1
+
+echo -e '\nCompiling...'
 su $RUN_AS -c "make $EXEC && ./$EXEC &"
